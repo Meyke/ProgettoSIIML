57b60f514beaf93cbb2dde883fd18eda42790604
Meyke <michele.tedesco487@virgilio.it>
2019-03-12 15:22:36 +0100

    aggiunto template recommender

---
+++ recommender/src/main/scala/Engine.scala
@@ -0,0 +1,39 @@
+package org.example.similarproduct
+
+import org.apache.predictionio.controller.EngineFactory
+import org.apache.predictionio.controller.Engine
+
+case class Query(
+  items: List[String],
+  num: Int,
+  categories: Option[Set[String]],
+  categoryBlackList: Option[Set[String]],
+  whiteList: Option[Set[String]],
+  blackList: Option[Set[String]]
+)
+
+case class PredictedResult(
+  itemScores: Array[ItemScore]
+){
+  override def toString: String = itemScores.mkString(",")
+}
+
+case class ItemScore(
+  item: String,
+  title: String,
+  date: String,
+  imdbUrl: String,
+  score: Double
+) extends Serializable
+
+object SimilarProductEngine extends EngineFactory {
+  def apply() = {
+    new Engine(
+      classOf[DataSource],
+      classOf[Preparator],
+      Map(
+        "als" -> classOf[ALSAlgorithm],
+        "cooccurrence" -> classOf[CooccurrenceAlgorithm]),
+      classOf[Serving])
+  }
+}
