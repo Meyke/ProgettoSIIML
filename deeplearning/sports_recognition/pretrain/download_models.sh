84a626b38e203d550a88a487cc34cc6cacc33918
Daniele Caldarini <caldarinidaniele@gmail.com>
2019-04-04 18:59:34 +0200

    remove directory

--- deeplearning/sports_recognition/pretrain/download_models.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Download the pre-trained TRN models
-echo 'Downloading TRNmultiscale on Moments in Time'
-wget http://relation.csail.mit.edu/models/TRN_moments_RGB_InceptionV3_TRNmultiscale_segment8_best.pth.tar
-wget http://relation.csail.mit.edu/models/moments_categories.txt
-
-
