30bda08e7f92133144ce9262ff089f10a268687e
Daniele Caldarini <caldarinidaniele@gmail.com>
2019-04-13 12:03:03 +0200

    updated some csv files

--- deeplearning/sports_recognition/create_csv_from_playlist.py
+++ deeplearning/sports_recognition/create_csv_from_playlist.py
@@ -6,10 +6,10 @@ SEPARATOR=","
 
 with open("videos.csv", "a") as file:
     writer = csv.writer(file)
-    plurl = "https://www.youtube.com/playlist?list=PLQrUWi5X12Nm0ZZvuiYN0dIReOJh1TJF6"
+    plurl = "https://www.youtube.com/playlist?list=PL-292yfpAFGb4_xI5kzFMppiv915n_FQI"
     playlist = pafy.get_playlist(plurl)
-    for i in range(20):
+    for i in range(12):
         url = playlist['items'][i]['pafy'].videoid
         print(url)
-        csv_line = "%s%s%s" % ("https://www.youtube.com/watch?v=" + url, SEPARATOR, "rowing")
+        csv_line = "%s%s%s" % ("https://www.youtube.com/watch?v=" + url, SEPARATOR, "fencing")
         writer.writerows([csv_line.split(',')])
